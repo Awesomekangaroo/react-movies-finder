d61b07b42b52ce41d3c60d727606cb335491c012
Eloy <eloy.lopezce@gmail.com>
2017-10-14 15:14:10 -0400

    Style navigation mobile header

--- app/components/Header.js
+++ app/components/Header.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import NavMenu from './NavMenu';
 
 class Header extends React.Component {
 	render() {
 		return(
 			<div className="nav-header">
-				Header
+				<div className="search-container">
+					<i className="fa fa-search" aria-hidden="hidden"></i>
+				</div>
+				<nav>
+					<div className="mobile-menu">
+						<i className="fa fa-bars" aria-hidden="hidden"></i>
+					</div>
+				</nav>
 			</div>
 		)
 	}
