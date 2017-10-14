51fd5309f579b7ded4a67a30083fba158b21c23d
Eloy <eloy.lopezce@gmail.com>
2017-10-13 20:43:14 -0400

    Init commit and setup of ReactJS

---
+++ app/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+// import { render } from 'react-dom';
+// var React = require('react');
+
+class App extends React.Component {
+	render() {
+		return(
+			<div>Hey!</div>
+		)
+	}
+}
+
+export default App;
