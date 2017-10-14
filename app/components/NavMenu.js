73c11414962f9925c9067fbce5427da655241ebb
Eloy <eloy.lopezce@gmail.com>
2017-10-14 15:13:53 -0400

    Add Navigation components

---
+++ app/components/NavMenu.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class NavMenu extends React.Component {
+	render() {
+		return(
+			<div className="nav-container">
+				
+			</div>
+		)
+	}
+}
+
+export default NavMenu;
