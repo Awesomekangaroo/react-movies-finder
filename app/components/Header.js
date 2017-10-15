5e7a20e9a3e6b76a3fa0cf40ab275483c28622ab
Eloy <eloy.lopezce@gmail.com>
2017-10-15 18:17:25 -0400

    Finalize mobile nav menu styling

--- app/components/Header.js
+++ app/components/Header.js
@@ -2,16 +2,28 @@ import React from 'react';
 import NavMenu from './NavMenu';
 
 class Header extends React.Component {
+	constructor() {
+		super();
+		this.state= { isActiveOn: false, isActiveOn2: true }
+		// this.addActiveClass = this.addActiveClass.bind(this);
+	}
+	addActiveClass(e) {
+		this.setState(prevState=> ({
+			isActiveOn: !prevState.isActiveOn,
+			isActiveOn2: !prevState.isActiveOn2
+		}));
+	}
 	render() {
 		return(
 			<div className="nav-header">
-				<div className="search-container">
+				<div className={"search-container" + (this.state.isActiveOn ? ' active' : '')} onClick={e=> this.addActiveClass(e)}>
 					<i className="fa fa-search" aria-hidden="hidden"></i>
 				</div>
 				<nav>
-					<div className="mobile-menu">
+					<div id="mobile-menu" className={this.state.isActiveOn2 ? 'active' : ''} onClick={e=> this.addActiveClass(e)} >
 						<i className="fa fa-bars" aria-hidden="hidden"></i>
 					</div>
+					<NavMenu />
 				</nav>
 			</div>
 		)
--- app/components/NavMenu.js
+++ app/components/NavMenu.js
@@ -3,8 +3,32 @@ import React from 'react';
 class NavMenu extends React.Component {
 	render() {
 		return(
-			<div className="nav-container">
-				
+			<div className="nav-menu__container">
+				<ul className="nav-menu__main">
+					<li>
+						<a href="">Search</a>
+					</li>
+					<li>
+						<a href="">Movies</a>
+					</li>
+					<li>
+						<a href="">Discover TV Shows</a>
+					</li>
+					<li>
+						<a href="">Latest</a>
+					</li>
+				</ul>
+				<ul className="nav-menu__about">
+					<li>
+						<a href="">About the API</a>
+					</li>
+					<li>
+						<a href="">More about developer</a>
+					</li>
+				</ul>
+				<div className="attribute-credits">
+					<img src="https://www.themoviedb.org/assets/static_cache/27b65cb40d26f78354a4ac5abf87b2be/images/v4/logos/powered-by-rectangle-green.svg" alt="The Movie DB logo"/>
+				</div>
 			</div>
 		)
 	}
