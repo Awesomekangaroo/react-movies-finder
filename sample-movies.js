a81efaf438d6c23ec8ca90257ebd42878fba0a89
Eloy <eloy.lopezce@gmail.com>
2017-10-28 20:44:15 -0400

    Add sample movies

---
+++ sample-movies.js
@@ -0,0 +1,56 @@
+module.exports = {
+	movie1: {
+		title: "Star Wars: The Last Jedi",
+		year: "December 15, 2017",
+		country: "USA",
+		rating: "PG-13",
+		media_type: "movie",
+		genre: "Action Adventure",
+		languages: [
+			"English",
+			"German",
+			"Spanish"
+		],
+		cast: {
+			"actors": [
+				{
+					"name": "Mark Hamill"
+				},
+				{
+					"name": "Daisey Ridley"
+				},
+				{
+					"name": "Adam Driver"
+				}
+			]
+		},
+		awards: [
+			{
+				"name": "Golden Trailers Awards"
+			}
+		],
+		posters: [
+			{
+				"mobile": "picture"
+			},
+			{
+				"tablet": "picture"
+			},
+			{
+				"desktop": "picture"
+			}
+		],
+		trailers: [
+			{
+				"id": 1,
+				"title": "Intro Trailer",
+				"url": "www.youtube.com",
+			},
+			{
+				"id": 2,
+				"title": "Intro Trailer 2",
+				"url": "www.google.com"
+			}
+		]
+	}
+}
